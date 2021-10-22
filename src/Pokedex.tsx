47363ce3579c78c702acc5f5057d1d2127a6cbf2
Aitor Urrutia <aitor.urrutia@biko2.com>
2021-10-22 19:25:34 +0200

    corrige maquetación

--- src/Pokedex.tsx
+++ src/Pokedex.tsx
@@ -3,10 +3,10 @@ import pokemon from './pokemon.json'
 
 export const Pokedex: React.FC = () => {
   return (
-    <>
+    <ul>
       {pokemon.results.map((result) => (
-        <span key={result.name}>{result.name}</span>
+        <li key={result.name}>{result.name}</li>
       ))}
-    </>
+    </ul>
   )
 }
