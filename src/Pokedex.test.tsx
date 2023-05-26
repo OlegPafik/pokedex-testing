de6e63507eb62dec4b2c5c902438a39de496fee1
Oleg <oleg.pafik@eversens.com>
2023-05-26 18:55:08 +0200

    Remove first tests thar now are unneeded

--- src/Pokedex.test.tsx
+++ src/Pokedex.test.tsx
@@ -3,22 +3,6 @@ import { Pokedex } from './Pokedex'
 import pokemons from './pokemon.json'
 
 describe('Pokédex', () => {
-  it('shows pokemon bulbasaur', () => {
-    render(<Pokedex />)
-
-    expect(screen.getByText('bulbasaur')).toBeInTheDocument()
-  })
-  it('shows pokemon ivysaur', () => {
-    render(<Pokedex />)
-
-    expect(screen.getByText('ivysaur')).toBeInTheDocument()
-  })
-  it('shows pokemon venusaur', () => {
-    render(<Pokedex />)
-
-    expect(screen.getByText('venusaur')).toBeInTheDocument()
-  })
-
   it('shows all pokemon', () => {
     render(<Pokedex />)
     pokemons.results.forEach(({ name }) => {
