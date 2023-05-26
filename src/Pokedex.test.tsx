26ab3527525c7a5ef2e6b05dca97b1d790525a9b
Oleg <oleg.pafik@eversens.com>
2023-05-26 18:07:41 +0200

    Add red test shows pokemon venusaur

--- src/Pokedex.test.tsx
+++ src/Pokedex.test.tsx
@@ -2,14 +2,19 @@ import { render, screen } from '@testing-library/react'
 import { Pokedex } from './Pokedex'
 
 describe('Pokédex', () => {
-  it('muestra pokemon bulbasaur', () => {
+  it('shows pokemon bulbasaur', () => {
     render(<Pokedex />)
 
     expect(screen.getByText('bulbasaur')).toBeInTheDocument()
   })
-  it('muestra pokemon ivysaur', () => {
+  it('shows pokemon ivysaur', () => {
     render(<Pokedex />)
 
     expect(screen.getByText('ivysaur')).toBeInTheDocument()
+  })
+  it('shows pokemon venusaur', () => {
+    render(<Pokedex />)
+
+    expect(screen.getByText('venusaur')).toBeInTheDocument()
   })
 })
