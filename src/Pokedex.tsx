5de620460b759656830cea8202471190f19ca195
Oleg <oleg.pafik@eversens.com>
2023-05-26 18:33:29 +0200

    Refactor into map from json

--- src/Pokedex.tsx
+++ src/Pokedex.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import pokemons from './pokemon.json'
 
 export const Pokedex: React.FC = () => {
+  const pokemonsList = pokemons.results
   return (
     <div>
-      <div>bulbasaur</div>
-      <div>ivysaur</div>
-      <div>venusaur</div>
+      {pokemonsList.map((pokemon) => (
+        <div> {pokemon.name} </div>
+      ))}
     </div>
   )
 }
